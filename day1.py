306940696788bf76a13c2b4fcda718c14aaaeecc
xcn7kjbw6k-cyber <xcn7kjbw6k@privaterelay.appleid.com>
2026-07-10 12:47:49 +0200

    feat: add day1 exercise

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("welcome to the Band name generator")
+city = input("what city do you live in?\n" )
+animal = input("whats the name of your animal?\n ")
+
+print("your band name could be " + city + " " + animal)
