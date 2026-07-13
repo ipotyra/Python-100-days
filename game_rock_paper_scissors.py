58f464c54e7c4e0f6df0be3bb7814167ad8da767
xcn7kjbw6k-cyber <xcn7kjbw6k@privaterelay.appleid.com>
2026-07-13 16:51:32 +0200

    feat: add rock paper scissors game

---
+++ game_rock_paper_scissors.py
@@ -0,0 +1,35 @@
+import random
+
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+random_game = [rock, paper, scissors]
+
+random_game = random.choice(random_game)
+print (input("What did you choose? rock, paper, or scissors?" ))
+print (random_game)
