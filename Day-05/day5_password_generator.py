35fc8570b20be485b777490037f48201f6c7e57e
xcn7kjbw6k-cyber <xcn7kjbw6k@privaterelay.appleid.com>
2026-07-27 12:42:29 +0200

    add loop while

--- Day-05/day5_password_generator.py
+++ Day-05/day5_password_generator.py
@@ -4,10 +4,19 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
+
+while True:
+    try:
+        nr_letters = int(input("How many letters would you like in your password?\n"))
+        if nr_letters < 0:
+            print("Please enter a positive number.")
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid number, not text.")
+
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-
 
 password = ""
 for char in range(0, nr_letters):
