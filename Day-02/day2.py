013085609a74af575c1ebca6d43c2188ffc41b04
xcn7kjbw6k-cyber <xcn7kjbw6k@privaterelay.appleid.com>
2026-07-27 11:13:44 +0200

    add whileloo

--- Day-02/day2.py
+++ Day-02/day2.py
@@ -1,7 +1,11 @@
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
+bill = float(input("What was the total bill? $\n"))
+tip = int(input("What percentage tip would you like to give? 10 12 15\n"))
+while tip not in [10, 12, 15]:
+    print("Invalid tip percentage. Please enter 10, 12, or 15.")
+    tip = int(input("What percentage tip would you like to give? 10 12 15\n"))
+
+people = int(input("How many people to split the bill?\n"))
 
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
